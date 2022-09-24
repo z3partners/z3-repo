0295db6f21a948104c293700760a39932107a961
z3 partners <z3partners.in@gmail.com>
2022-09-24 22:50:22 +0530

    css changes

--- public/assets/js/z3.core.js
+++ public/assets/js/z3.core.js
@@ -1,28 +1,38 @@
-function delCat(cat) {
-    const catDetails = JSON.parse(cat);
-    console.log(catDetails.category_id);
-}
-
 $(document).ready(function () {
 
     $(".edit-cat-btn").click(function (e) {
-        const catDetails = JSON.parse(e.target.dataset.category);
-        console.log(catDetails);
+        const elm = e.target.closest(".edit-cat-btn");
+        const catDetails = JSON.parse(elm.dataset.category);
         document.getElementById("addLink").style.display = "inline-flex";
         document.getElementById("category").value = catDetails.category_name;
         document.getElementById("catId").value = catDetails.category_id;
         document.getElementById("flexSwitchCheckChecked").checked = catDetails.status ? "checked" : '';
         document.getElementById("catBtn").innerHTML = "Edit";
     });
-/*
-    $("#submit").click(function () {
-        $.post("/request",
-            {
-                name: "viSion",
-                designation: "Professional gamer"
-            },
-            function (data, status) {
-                console.log(data);
-            });
-    });*/
+
+    $(".del-cat-btn").click(function (e) {
+        const elm = e.target.closest(".del-cat-btn");
+        const catDetails = JSON.parse(elm.dataset.category);
+        const res = confirm(`Are your sure to DELETE ${catDetails.category_name}`);
+        if (res) {
+            $.post("/del-category", {
+                    catId: catDetails.category_id
+                },
+                function (data, status) {
+                    console.log(data);
+                });
+        }
+    });
+
+    /*
+     $("#submit").click(function () {
+     $.post("/request",
+     {
+     name: "viSion",
+     designation: "Professional gamer"
+     },
+     function (data, status) {
+     console.log(data);
+     });
+     });*/
 });
