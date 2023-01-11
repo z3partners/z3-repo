120f58f381e7db10eedf0969d4e4d2177913b8fa
z3 partners <z3partners.in@gmail.com>
2023-01-11 21:18:49 +0530

    updated db hostname

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const configData = require('./config.json');
 const dbPassword = configData.password.db;
-
 const config = {
     db: {
-        host: "179.61.188.50",
-        user: "irpo_z3partners-prod",
+        host: "localhost",
+        user: "emofmimy",
         password: dbPassword,
-        database: "irpo_z3partners",
+        database: "irpo_z3partners"
     }
 };
+
 module.exports = config;
