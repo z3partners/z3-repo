0983d8e6654c851eeb1a24c16b11da60fdd7bc96
Anoop <anoop@4thdimension.in>
2023-04-29 15:25:40 +0530

    Sub category search bug fix

--- routes/investor/investorHome.js
+++ routes/investor/investorHome.js
@@ -51,7 +51,7 @@ router.post('/', async function(req, res, next) {
 
     try {
         let searchParams = {};
-        //console.log(req.session.users);
+        // console.log(req.body);
         res.locals.showFilter =  true;
         let docCat = "-1";
         searchParams.investor_id = req.session.users.user_id;
@@ -66,12 +66,14 @@ router.post('/', async function(req, res, next) {
         const end_date= req.body.end_date;
         const quarter = req.body.quarter;
         const category_id = req.body.selectCat;
+        const sub_category_id = req.body.selectSubCat;
         if((start_date === '' && end_date !=='') || !isValidateDate(start_date, end_date)) {
             req.session.msg = 'Please choose validate date range';
         } else {
             (start_date && end_date ) ? searchParams.date_range = ` between '${start_date} 00:00:00' and '${end_date} 23:59:59' `: '';
             quarter ? searchParams.quarter = quarter : '';
             category_id ? searchParams.category_id = category_id : '';
+            sub_category_id ? searchParams.sub_category_id = sub_category_id : '';
         }
         const resposne = await categoryService.listCategory();
         const resAll = await categoryService.listAll();
